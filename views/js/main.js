0f1f479a75a3e4baaeee6fb0a91dee687eb974fe
tejas-kale <kaletejas2006@gmail.com>
2016-03-01 19:15:17 +0530

    Layout trashing removed

--- views/js/main.js
+++ views/js/main.js
@@ -528,6 +528,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    elem.style.willChange = 'transform'
     document.querySelector("#movingPizzas1").appendChild(elem);
   }
   updatePositions();

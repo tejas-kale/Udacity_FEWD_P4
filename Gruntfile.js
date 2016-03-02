e4e68cda2feb16163c6e5e35eb451367511940cd
tejas-kale <kaletejas2006@gmail.com>
2016-03-02 19:04:19 +0530

    grunt-pagespeed-ngrok support in progress

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,6 @@
+// Load 'ngrok' module
+var ngrok = require('ngrok');
+
 // wrapper function
 module.exports = function(grunt){
 	// load all our Grunt plugins
@@ -83,11 +86,45 @@ module.exports = function(grunt){
         			dest: 'dist/views/'
         		}]
         	}
+        },
+
+        pagespeed: {
+        	options: {
+        		nokey: true,
+        		locale: 'en_GB',
+        		threshold: 40
+        	},
+        	local: {
+        		options: {
+        			strategy: 'desktop'
+        		}
+        	},
+        	mobile: {
+        		options: {
+        			strategy: 'mobile'
+        		}
+        	}
         }
 
     });
 
+	// Register task for 'ngrok'
+	grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function () {
+		var done = this.async();
+		var port = 8000;
+
+		ngrok.connect (port, function (err, url) {
+			if (err !== null) {
+				grunt.fail.fatal(err);
+				return done();
+			}
+			grunt.config.set('pagespeed.options.url', url);
+			grunt.task.run('pagespeed');
+			done();
+		});
+	});
+
     // define the default task that executes when we run 'grunt' from inside the project
-    grunt.registerTask('default', ['jshint', 'imagemin', 'uglify', 'cssmin', 'htmlmin']);
+    grunt.registerTask('default', ['jshint', 'imagemin', 'uglify', 'cssmin', 'htmlmin', 'psi-ngrok']);
 
 };
